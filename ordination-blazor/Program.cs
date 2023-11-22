b42e8294ecb297fb8759513afd1a39f658b578ce
DenGuleDansker <chilinhm@gmail.com>
2023-11-22 12:12:50 +0100

    ændring

--- ordination-blazor/Program.cs
+++ ordination-blazor/Program.cs
@@ -8,6 +8,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+
 builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<ApiService>();
 
